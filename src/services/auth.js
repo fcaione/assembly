1ae71966a21785723bb4e9ee6f828b7db9bdeda3
Freddy Caione <fac299@nyu.edu>
2023-04-03 16:50:12 -0400

    changed baseURL

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,6 @@
 import Axios from 'axios'
 
-export const BASE_URL = 'https://assembly-api.herokuapp.com/'
+export const BASE_URL = 'https://assembly-api.herokuapp.com'
 // export const BASE_URL = 'http://localhost:3001/api'
 
 const Client = Axios.create({ baseURL: BASE_URL })
